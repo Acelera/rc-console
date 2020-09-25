a65b4bbde991999263fca4e7491cee7ea684b4e5
marcus <marcus@acelera.cloud>
2020-09-25 12:04:44 -0300

    [ADD] #test - Adicionei um teste de console.

---
+++ src/Test.js
@@ -0,0 +1,9 @@
+import Console from './Console';
+
+Console.newLine();
+Console.log('Mensagem do tipo Log!');
+Console.info('Mensagem do tipo INFORMAÇÃO!');
+Console.war('Mensagem do tipo ALERTA!');
+Console.err('Mensagem do tipo ERRO!');
+Console.ok('Mensagem do tipo SUCESSO!');
+Console.newLine();
