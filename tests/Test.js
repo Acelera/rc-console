69478e04162b34347a6458fdb1c84fc5c135be5b
Marcus Vinícius <marcus@pop-os.localdomain>
2020-10-06 17:54:05 -0300

    v0.6.0

---
+++ tests/Test.js
@@ -0,0 +1,9 @@
+import Log from '../dist';
+
+Log.newLine();
+Log.log('Mensagem do tipo Log!');
+Log.info('Mensagem do tipo INFORMAÇÃO!');
+Log.war('Mensagem do tipo ALERTA!');
+Log.err('Mensagem do tipo ERRO!');
+Log.ok('Mensagem do tipo SUCESSO!');
+Log.newLine();
